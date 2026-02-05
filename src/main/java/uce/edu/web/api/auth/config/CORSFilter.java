a3b8fb5bff8a849b54177c8f502237a4579aa48a
Xavier Tapia <fxtapiar@uce.edu.ec>
2026-02-05 14:07:43 -0500

    TAREA 7: OAuth 2.0

--- src/main/java/uce/edu/web/api/auth/config/CORSFilter.java
+++ src/main/java/uce/edu/web/api/auth/config/CORSFilter.java
@@ -9,24 +9,24 @@ import java.io.IOException;
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
 
-    @Override
-    public void filter(ContainerRequestContext requestContext,
-            ContainerResponseContext responseContext) throws IOException {
+        @Override
+        public void filter(ContainerRequestContext requestContext,
+                        ContainerResponseContext responseContext) throws IOException {
 
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Origin",
-                "*");
+                responseContext.getHeaders().add(
+                                "Access-Control-Allow-Origin",
+                                "*");
 
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Credentials",
-                "true");
+                responseContext.getHeaders().add(
+                                "Access-Control-Allow-Credentials",
+                                "true");
 
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Headers",
-                "origin, content-type, accept, authorization, x-requested-with");
+                responseContext.getHeaders().add(
+                                "Access-Control-Allow-Headers",
+                                "origin, content-type, accept, authorization, x-requested-with");
 
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Methods",
-                "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH");
-    }
+                responseContext.getHeaders().add(
+                                "Access-Control-Allow-Methods",
+                                "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH");
+        }
 }
